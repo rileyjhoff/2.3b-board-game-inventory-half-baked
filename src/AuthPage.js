7cf5f026185130cdc4fb45b6317539b92b7425b7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-11 14:35:20 -0700

    AuthPage keeps track of formEmail and formPassword state. input fields are assigned values equal to react state. on input change, formEmail and formPassword state is updated. on form submit, either handleSignIn or handleSignUp is called, which gets the user info from signIn and signUp functions and sets the current user state. upon user state updating, the user is redirected to the board game list

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,41 +1,61 @@
 import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils.js';
 
-export default function AuthPage(props) {
+export default function AuthPage({ setUser }) {
   // you'll need to track the form state of the email and password
+  const [formEmail, setFormEmail] = useState();
+  const [formPassword, setFormPassword] = useState();
 
   async function handleSignIn(e) {
     e.preventDefault();
-      
     // sign the user in using the form state
-
+    const currUser = await signIn(formEmail, formPassword);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
+    setUser(currUser);
   }
-    
-  async function handleSignUp() {
-    // sign the user up using the form state
 
+  async function handleSignUp(e) {
+    e.preventDefault();
+    // sign the user up using the form state
+    const currUser = await signUp(formEmail, formPassword);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
+    setUser(currUser);
   }
 
   return (
-    <div className='auth'>
-      <h1><em>Boardzo</em></h1>
+    <div className="auth">
+      <h1>
+        <em>Boardzo</em>
+      </h1>
       {/* on submit, sign the user in using the function defined above */}
       <form>
         <label>
-            Email
+          Email
           {/* on change, update the form state for email */}
-          <input required type="email" name="email" />
+          <input
+            required
+            type="email"
+            name="email"
+            value={formEmail}
+            onChange={(e) => setFormEmail(e.target.value)}
+          />
         </label>
         <label>
-            Password
+          Password
           {/* on change, update the form state for password */}
-          <input required type="password" name="password" />
+          <input
+            required
+            type="password"
+            name="password"
+            value={formPassword}
+            onChange={(e) => setFormPassword(e.target.value)}
+          />
         </label>
-        <button>Sign In</button>
+        <button onClick={handleSignIn}>Sign In</button>
         {/* on clicking sign up, sign the user up using the function defined above */}
-        <button type="button" >Sign Up</button>
+        <button type="button" onClick={handleSignUp}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
