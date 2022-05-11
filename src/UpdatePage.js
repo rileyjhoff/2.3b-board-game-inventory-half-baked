4faba1f4a8bc640b8b3be7314f3a657190a4273d
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-11 15:34:23 -0700

    replaced game detail on UpdatePage with a form to update the game. the form is prefilled with game info and updates game state whenever an input is changed. on form submit, handleSubmit is called, which updates the game in the database and redirect the user back to the list page

--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -23,13 +23,72 @@ export default function UpdatePage() {
   }
 
   return (
-    <div className="detail">
-      <h1>{game.title}</h1>
-      <h2>
-        A {game.genre} game for {game.min_players} to {game.max_players} players
-      </h2>
-      <h3>by celebrated designer {game.designer}</h3>
-      <p>{game.description}</p>
+    <div className="update">
+      <form onSubmit={handleSubmit}>
+        <h2>Update board game</h2>
+        <label>
+          Title
+          <input
+            required
+            name="title"
+            value={game.title}
+            onChange={(e) => setGame({ ...game, title: e.target.value })}
+          />
+        </label>
+        <label>
+          Genre
+          <select
+            required
+            value={game.genre}
+            onChange={(e) => setGame({ ...game, genre: e.target.value })}
+          >
+            <option>Tile-laying</option>
+            <option>Economic</option>
+            <option>War</option>
+            <option>Card</option>
+            <option>Abstract</option>
+            <option>Cooperative</option>
+            <option>Solo</option>
+          </select>
+        </label>
+        <label>
+          Designer
+          <input
+            required
+            name="designer"
+            value={game.designer}
+            onChange={(e) => setGame({ ...game, designer: e.target.value })}
+          />
+        </label>
+        <label>
+          Min Players
+          <input
+            required
+            name="min_players"
+            value={game.min_players}
+            onChange={(e) => setGame({ ...game, min_players: e.target.value })}
+          />
+        </label>
+        <label>
+          Max Players
+          <input
+            required
+            name="max_players"
+            value={game.max_players}
+            onChange={(e) => setGame({ ...game, max_players: e.target.value })}
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            required
+            name="description"
+            value={game.description}
+            onChange={(e) => setGame({ ...game, description: e.target.value })}
+          />
+        </label>
+        <button>Update game</button>
+      </form>
     </div>
   );
 }
